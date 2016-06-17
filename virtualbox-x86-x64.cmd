0ac27f8f92107723fbba4295401492acf645b94f
catonrug <aigars.kadikis@gmail.com>
2016-06-17 15:30:14 +0300

    added virtualbox silent install

--- virtualbox-x86-x64.cmd
+++ virtualbox-x86-x64.cmd
@@ -1,1 +1,2 @@
 for /f "tokens=*" %%a in ('wget --no-check-certificate -qO- "https://www.virtualbox.org/wiki/Downloads" ^| sed "s/\d034/\n/g" ^| grep "exe" ^| head -1') do wget --no-check-certificate %%a -P %1
+echo for /f "tokens=*" %%%%a in ('dir /b "%%~dp0VirtualBox*.exe"') do start /wait "" "%%~dp0%%%%a" --silent>> "%1\FirstRun.bat"
