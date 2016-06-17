d9409047f3c1696d08c15403b032506b99d61a61
catonrug <aigars.kadikis@gmail.com>
2016-06-17 15:03:12 +0300

    next

--- 32-bit-re-download.cmd
+++ 32-bit-re-download.cmd
@@ -1,3 +1,4 @@
 @echo off
 if not exist "%userprofile%\Desktop\x86\FirstRun" md "%userprofile%\Desktop\x86\FirstRun"
+echo @echo off > "%userprofile%\Desktop\x86\FirstRun\FirstRun.bat"
 for /f "tokens=*" %%a in ('dir /b "*x86*cmd"') do call "%%a" %userprofile%\Desktop\x86\FirstRun
---
+++ winscp-x86-x64.cmd
@@ -0,0 +1,2 @@
+for /f "tokens=*" %%a in ('wget --no-check-certificate -qO- "https://winscp.net/eng/download.php" ^| sed "s/\d034/\n/g" ^| grep "winscp.*setup.*exe$" ^| sed "s/\.\./https:\/\/winscp\.net/"') do wget --no-check-certificate "%%a" -P %1
+echo for /f "tokens=*" %%%%a in ('dir /b "%%~dp0winscp*setup.exe"') do start /wait "" "%%~dp0%%%%a" /S >> "%1\FirstRun.bat"
