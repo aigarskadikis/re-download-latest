4d7cdbe22786e5ab38aaeea4f5370b99f4b846ab
catonrug <aigars.kadikis@gmail.com>
2016-06-17 19:16:54 +0300

    added Win32DiskImager

---
+++ win32diskimager-x86-x64.cmd
@@ -0,0 +1,2 @@
+for /f "tokens=*" %%a in ('wget --no-check-certificate -qO- "https://sourceforge.net/projects/win32diskimager/rss?path=/Archive" ^| grep "link.*Win32DiskImager.*install\.exe" ^| head -1 ^| sed "s/^.*http/http/g;s/exe.*$/exe/g"') do wget --no-check-certificate %%a -P %1
+echo for /f "tokens=*" %%%%a in ('dir /b "%%~dp0Win32DiskImager*.exe"') do start /wait "" "%%~dp0%%%%a" /SILENT>> "%1\FirstRun.bat"
